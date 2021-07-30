c206bf5ee5f8d2c081a372ce4c9e0d8f0eca862f
Sam Littlefair <sam.littlefair@prismic.io>
2021-07-30 14:25:52 +0100

    Create SliceZone and Client

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,41 +1,63 @@
 import { createClient, getEndpoint } from '@prismicio/client'
 import { asText, asHTML, asLink, asDate } from '@prismicio/helpers'
-import SliceZone from './SliceZone.svelte'
 import MagicString from 'magic-string'
+import util from 'util'
 
-// const addHeadersToClient = (endpoint, session, fetch) => {
-//   const { cookie } = session
-//   const req = {
-//     headers: {
-//       cookie,
-//     },
-//   }
-//   const client = createClient(endpoint, { fetch, req })
-//   return client
-// }
+const addHeadersToClient = (endpoint, session, fetch) => {
+  const { cookie } = session
+  const req = {
+    headers: {
+      cookie,
+    },
+  }
+  const client = createClient(endpoint, { fetch, req })
+  return client
+}
 
-// const usePrismic = ({ repoName }) => {
-//   const endpoint = getEndpoint(repoName)
-//   const Client = (session = null, fetch = null) =>
-//     addHeadersToClient(endpoint, session, fetch)
-
-//   return {
-//     markup: ({ content, filename }) => {
-//       const pos = content.indexOf('SliceZone')
-//       if (pos < 0) {
-//         return { code: content }
-//       }
-//       const s = new MagicString(content, { filename })
-//       s.prepend("import { SliceZone } from 'prismic-svelte'; ")
-//       let code = s.toString()
-//       let map = s.generateMap()
-//       return {
-//         code,
-//         map,
-//       }
-//     },
-//   }
-// }
+const usePrismic = ({ repoName }) => {
+  return {
+    script: ({ content, filename, markup, attributes }) => {
+      if (attributes.context === 'module') {
+        const s = new MagicString(content, { filename })
+        const sliceZonePos = markup.indexOf('SliceZone')
+        const prismicPos = markup.indexOf('prismic')
+        const clientPos = markup.indexOf('Client')
+        if (sliceZonePos >= 0) {
+          s.prepend(`import SliceZone from 'prismic-svelte/SliceZone.svelte'; `)
+        }
+        if (prismicPos >= 0) {
+          s.prepend(
+            `import { prismic } from 'prismic-svelte'; prismic.client = prismic.Client("${repoName}"); `,
+          )
+        }
+        if (prismicPos >= 0 || sliceZonePos >= 0) {
+          return {
+            code: s.toString(),
+            map: s.generateMap(),
+          }
+        }
+      }
+      return {
+        code: content,
+      }
+    },
+  }
+}
 
 // export { SliceZone, asText, asHTML, asLink, asDate, usePrismic }
-export { SliceZone }
+
+const Client = (repoName) => {
+  const endpoint = getEndpoint(repoName)
+  return (session = null, fetch = null) =>
+    addHeadersToClient(endpoint, session, fetch)
+}
+
+const prismic = {
+  asText,
+  asHTML,
+  asLink,
+  asDate,
+  Client,
+}
+
+export { usePrismic, prismic }
